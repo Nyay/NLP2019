91b7a54bfa2aa6c46cc9c8045e267ceeeb2ef646
Dima Tatarinov <frickieflop@gmail.com>
2018-11-25 20:46:13 +0300

    Новое домашнее задание

---
+++ ipython_log.py
@@ -0,0 +1,9 @@
+# IPython log file
+
+print
+get_ipython().magic('logon')
+get_ipython().magic('logstart')
+get_ipython().magic('logon')
+print()
+get_ipython().magic('logstate')
+get_ipython().magic('logstop')
